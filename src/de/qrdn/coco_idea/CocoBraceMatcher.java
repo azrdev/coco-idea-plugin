c6690b1a425bccd6c17c7ebdb2305061c510349f
jBiegert <azrdev@qrdn.de>
2016-12-24 17:24:16 +0100

    Implement Brace matching

---
+++ src/de/qrdn/coco_idea/CocoBraceMatcher.java
@@ -0,0 +1,36 @@
+package de.qrdn.coco_idea;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import de.qrdn.coco_idea.psi.CocoTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CocoBraceMatcher implements PairedBraceMatcher {
+
+    private static final BracePair[] PAIRS = new BracePair[]{
+            new BracePair(CocoTypes.LPAREN, CocoTypes.RPAREN, false),
+            new BracePair(CocoTypes.LAPAREN, CocoTypes.RAPAREN, false),
+            new BracePair(CocoTypes.LSPAREN, CocoTypes.RSPAREN, false),
+            new BracePair(CocoTypes.LESS, CocoTypes.GREATER, false),
+            new BracePair(CocoTypes.LESSDOT, CocoTypes.GREATERDOT, false),
+            new BracePair(CocoTypes.INSTR_START, CocoTypes.INSTR_END, false),
+    };
+
+    @Override
+    public BracePair[] getPairs() {
+        return PAIRS;
+    }
+
+    @Override
+    public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType iElementType, @Nullable IElementType iElementType1) {
+        return true;
+    }
+
+    @Override
+    public int getCodeConstructStart(PsiFile psiFile, int openingBraceOffset) {
+        return openingBraceOffset;
+    }
+}
