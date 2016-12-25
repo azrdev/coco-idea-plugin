45f8c027e703dbb7a4da72f5fab4ace398876471
jBiegert <azrdev@qrdn.de>
2016-12-25 19:10:00 +0100

    References: implement usage references (PsiReference). still defunct

---
+++ src/de/qrdn/coco_idea/CocoReference.java
@@ -0,0 +1,37 @@
+package de.qrdn.coco_idea;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiReferenceBase;
+import com.intellij.util.ArrayUtil;
+import de.qrdn.coco_idea.psi.CocoFile;
+import de.qrdn.coco_idea.psi.CocoIdentRule;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class CocoReference extends PsiReferenceBase<CocoIdentRule> {
+
+    public CocoReference(CocoIdentRule element) {
+        super(element);
+    }
+
+    @Nullable
+    @Override
+    public PsiElement resolve() {
+        PsiFile containingFile = myElement.getContainingFile();
+        String referenceName = getRangeInElement().substring(myElement.getText());
+        if(containingFile instanceof CocoFile) {
+            return resolve((CocoFile)containingFile, referenceName);
+        }
+        return null;
+    }
+
+    @Nullable
+    public abstract PsiElement resolve(@NotNull CocoFile containingFile, String referenceName);
+
+    @NotNull
+    @Override
+    public Object[] getVariants() {
+        return ArrayUtil.EMPTY_OBJECT_ARRAY;
+    }
+}
