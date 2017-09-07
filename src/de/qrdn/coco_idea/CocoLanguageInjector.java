a51285271716750a4e8b099a02337d1e9db1f034
azrdev <azrdev@qrdn.de>
2017-09-07 22:48:04 +0200

    Use a single token for INSTRUMENTATION_CODE, and separate ones for delimiters
    Adapted cherry-pick of 2e66ce6113092604ab8feb70bc2583f6baa6d4c4

--- src/de/qrdn/coco_idea/CocoLanguageInjector.java
+++ src/de/qrdn/coco_idea/CocoLanguageInjector.java
@@ -5,20 +5,17 @@ import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.psi.InjectedLanguagePlaces;
 import com.intellij.psi.LanguageInjector;
 import com.intellij.psi.PsiLanguageInjectionHost;
-import de.qrdn.coco_idea.psi.CocoInstrumentationCode;
+import de.qrdn.coco_idea.psi.CocoInstrumentationCodeRule;
 import org.jetbrains.annotations.NotNull;
 
 public class CocoLanguageInjector implements LanguageInjector {
     @Override
     public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost host, @NotNull InjectedLanguagePlaces injectionPlacesRegistrar) {
         //TODO: distinguish injection types / contexts
-        if(host instanceof CocoInstrumentationCode) {
+        if(host instanceof CocoInstrumentationCodeRule) {
             //TODO: configure instrumentation language
             LanguageFileType maybeJavaType = (LanguageFileType) FileTypeManager.getInstance().getStdFileType("JAVA");
-            injectionPlacesRegistrar.addPlace(
-                    maybeJavaType.getLanguage(),
-                    ((CocoInstrumentationCode) host).getRelevantTextRange(),
-                    "{", "}");
+            injectionPlacesRegistrar.addPlace(maybeJavaType.getLanguage(), host.getTextRange(),"{", "}");
         }
     }
 }
