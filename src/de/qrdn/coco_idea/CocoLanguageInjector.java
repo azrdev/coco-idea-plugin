fb7598d4916eaf3a422a568445502f34d31dd4fd
azrdev <azrdev@qrdn.de>
2017-09-11 15:36:19 +0200

    WIP: implement MultiHostInjector to have fragments know each other
    does not work yet, because startInjecting().doneInjecting() surround
    each fragment, instead of being called once around all fragments

--- src/de/qrdn/coco_idea/CocoLanguageInjector.java
+++ src/de/qrdn/coco_idea/CocoLanguageInjector.java
@@ -1,43 +1,57 @@
 package de.qrdn.coco_idea;
 
 import com.intellij.lang.Language;
+import com.intellij.lang.injection.MultiHostInjector;
+import com.intellij.lang.injection.MultiHostRegistrar;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.InjectedLanguagePlaces;
-import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.templateLanguages.TemplateDataLanguageMappings;
 import de.qrdn.coco_idea.psi.*;
 import org.jetbrains.annotations.NotNull;
 
-public class CocoLanguageInjector implements LanguageInjector {
+import java.util.Collections;
+import java.util.List;
+
+public class CocoLanguageInjector implements MultiHostInjector {
+
     @Override
-    public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost host,
-                                     @NotNull InjectedLanguagePlaces injectionPlacesRegistrar) {
-        if (!(host instanceof CocoInstrumentationCodeRule))
+    public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
+        if (!(context instanceof CocoInstrumentationCodeRule))
             return;
         //TODO: offer selecting instrumentation language from context menu like ConfigurableTemplateLanguageFileViewProvider (the template lang settings work already)
-        final Language language = TemplateDataLanguageMappings.getInstance(host.getProject())
-                .getMapping(host.getContainingFile().getVirtualFile());
+        final Language language = TemplateDataLanguageMappings.getInstance(context.getProject())
+                .getMapping(context.getContainingFile().getVirtualFile());
         if (language == null)
             return;
-        //FIXME: injection places should know about each other, e.g. for name resolution
         //TODO: glue should be language-dependent
-        String prefix = "", suffix = "";
-        if (host instanceof CocoIncludeHeader) {
+        final String prefix, suffix;
+        if (context instanceof CocoIncludeHeader) {
             prefix = "";
             suffix = "public class Parser {";
-        } else if (host instanceof CocoGlobalDecls) {
+        } else if (context instanceof CocoGlobalDecls) {
             prefix = suffix = "";
-        } else if (host instanceof CocoProduction) {
-            prefix = "public void " + ((CocoProduction) host).getName() + "(";
+        } else if (context instanceof CocoProduction) {
+            prefix = "public void " + ((CocoProduction) context).getName() + "(";
             suffix = ") {";
-        } else if (host instanceof CocoSemText) {
+        } else if (context instanceof CocoSemText) {
             prefix = suffix = "";
-        } else if (host instanceof CocoSymRef) {
-            prefix = ((CocoSymRef) host).getName() + "(";
+        } else if (context instanceof CocoSymRef) {
+            prefix = ((CocoSymRef) context).getName() + "(";
             suffix = ");";
+        } else {
+            prefix = suffix = null;
         }
-        injectionPlacesRegistrar.addPlace(language,
-                TextRange.from(1, host.getTextLength() -1), prefix, suffix);
+        registrar
+                .startInjecting(language)
+                .addPlace(prefix, suffix, (PsiLanguageInjectionHost) context,
+                          TextRange.from(1, context.getTextLength() -1))
+                .doneInjecting();
+    }
+
+    @NotNull
+    @Override
+    public List<? extends Class<? extends PsiElement>> elementsToInjectIn() {
+        return Collections.singletonList(CocoInstrumentationCodeRule.class);
     }
 }
